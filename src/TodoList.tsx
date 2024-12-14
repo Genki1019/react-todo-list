f6fe46d6faf6ccb858680c1f1aee1a0d59f4cfa3
Genky1019 <genky1019@gmail.com>
2024-12-14 20:28:41 +0900

    feat: 期限が近いタスクの色分けを実装

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -18,32 +18,65 @@ function TodoList({
   handleEdit,
   handleDelete,
 }: TodoListProps) {
+  const getDiffInDays = (deadline: string): number => {
+    const today = new Date();
+    const deadlineDate = new Date(deadline);
+    return Math.ceil(
+      (deadlineDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
+    );
+  };
+
+  const getFontColor = (diffInDays: number): string => {
+    if (diffInDays <= 1) return "#f93840";
+    if (diffInDays <= 3) return "#c98d00";
+    return "";
+  };
+
+  const getBackgroundColor = (diffInDays: number): string => {
+    if (diffInDays <= 1) return "#feeff1";
+    if (diffInDays <= 3) return "#fcfae1";
+    return "";
+  };
+
   return (
     <ul className="todoList">
-      {todos.map((todo) => (
-        <li key={todo.id} className="todoContent">
-          <input
-            type="checkbox"
-            className="checkbox"
-            checked={todo.isCompleted}
-            onChange={() => handleCheck(todo.id)}
-          />
-          <input
-            type="text"
-            className="todoTitle"
-            value={todo.title}
-            onChange={(e) => handleEdit(todo.id, e.target.value)}
-            disabled={todo.isCompleted}
-          />
-          <button
-            type="button"
-            className="deleteButton"
-            onClick={() => handleDelete(todo.id)}
-          >
-            <img src="/delete-btn.svg" alt="削除" />
-          </button>
-        </li>
-      ))}
+      {todos.map((todo) => {
+        const diffInDays = getDiffInDays(todo.deadline);
+        const fontColor = getFontColor(diffInDays);
+        const backgroundColor = getBackgroundColor(diffInDays);
+
+        return (
+          <li key={todo.id} className="todoContent">
+            <input
+              type="checkbox"
+              className="checkbox"
+              checked={todo.isCompleted}
+              onChange={() => handleCheck(todo.id)}
+              style={{
+                backgroundColor: backgroundColor,
+              }}
+            />
+            <input
+              type="text"
+              className="todoTitle"
+              value={todo.title}
+              onChange={(e) => handleEdit(todo.id, e.target.value)}
+              disabled={todo.isCompleted}
+              style={{
+                color: fontColor,
+                backgroundColor: backgroundColor,
+              }}
+            />
+            <button
+              type="button"
+              className="deleteButton"
+              onClick={() => handleDelete(todo.id)}
+            >
+              <img src="/delete-btn.svg" alt="削除" />
+            </button>
+          </li>
+        );
+      })}
     </ul>
   );
 }
